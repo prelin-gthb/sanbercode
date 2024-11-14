91a95addc13ebfa10d89cedf522e3abdd422374b
prelin-gthb <144567966+prelin-gthb@users.noreply.github.com>
2024-11-15 05:17:15 +0700

    Update soal1.js
    Add a case when the input is not a number.

--- soal1.js
+++ soal1.js
@@ -1,17 +1,21 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 // Meminta user untuk memasukkan angka
-const num = prompt('Masukkan sebuah angka: '); 
+const num = prompt('Masukkan sebuah angka: ');
 
+// Mengecek apakah inputan bukan angka
+if (isNaN(num)) {
+    console.log('Input harus berupa angka.');
+}
 // Mengecek apakah angka yang dimasukkan adalah bilangan negatif
-if (Number(num) < 0) {
+else if (Number(num) < 0) {
     console.log('Tidak bisa input bilangan negatif');
 } 
 // Mengecek apakah angka yang dimasukkan adalah bilangan ganjil
 else if (Number(num) % 2 !== 0) {
     console.log('Tidak bisa input bilangan ganjil');
 } 
-// Mengecek jika angka yang diiput valid (bilangan genap & bukan bilangan negatif)
+// Mengecek jika angka yang diinput valid (bilangan genap & bukan bilangan negatif)
 else {
     const result = Math.sqrt(Number(num)); // Menghitung akar pangkat 2 dari angka yang diinput
     console.log(`Akar pangkat 2 dari ${num} adalah ${result}`);
